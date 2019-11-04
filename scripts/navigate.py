274bfa489fbb4f4954cd49a3d7c68828d9e82582
Alex LaGrassa <lagrassa@mit.edu>
2019-11-04 13:21:45 -0500

    dmp fitting

--- scripts/navigate.py
+++ scripts/navigate.py
@@ -6,6 +6,41 @@ from motion_planners.rrt_connect import birrt
 ne = NavEnv(slip=True)
 
 distance = lambda x,y: 1
+
+def closest_pt_index(path, state):
+    dists = np.linalg.norm(path-state,axis=1)
+    min_dist_pt = np.argmin(dists)
+    return min_dist_pt
+
+def select_imitation_traj(state, history):
+    #which point is the closest distance 
+    min_dists = []
+    for path in history.paths:
+        min_dist_pt = closest_pt_index(path, state)
+        min_dists.append(min_dist_pt)
+    best_path_idx = np.argmin(min_dists)
+    closest_pt_idx = closest_pt_index(history.paths[best_path_idx],state)
+    return history.paths[best_path_idx][closest_pt_idx:]
+
+
+def moving_average(a, n=5) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+def imitate_history(history, state):
+    import pydmps
+    #find trajectory with the closest state to this one, form a DMP starting from that state to imitate it
+    dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=2, n_bfs=20, ay=np.ones(2)*25.0, dt =1/186.)
+    imitation_traj = select_imitation_traj(state, history)
+    imitation_traj = np.vstack([moving_average(np.array(imitation_traj)[:,i] ) for i in range(0,2)])
+    y_des = imitation_traj
+    
+    dmp.imitate_path(y_des=y_des, plot=False)
+    y_track, dy_track, ddy_track = dmp.rollout(timesteps=210)
+    return y_track
+
 def sample():
     x_rand = np.random.randint(low=0,high=ne.gridsize[0])
     y_rand = np.random.randint(low=0,high=ne.gridsize[0])
@@ -43,12 +78,19 @@ def interp(history, s0):
         
     return path
 
+def print_path_stats(path):
+    max_x_diff = np.max(np.diff(np.array(path)[:,0]))
+    max_y_diff = np.max(np.diff(np.array(path)[:,1]))
 
+    print("diff", abs(max_x_diff-max_y_diff))
+    print("max x diff", max_x_diff)
+    print("may y diff", max_y_diff)
 
 start = ne.pos
 goal = ne.goal
 starts = [(0,3),(1,1),(2,4),(5,6),(4,6),(9,10)]
 history = History()
+thresh = 3
 for i in range(len(starts)):
     ne.pos = np.array(starts[i])
     start = np.array(starts[i]).copy()
@@ -56,12 +98,27 @@ for i in range(len(starts)):
     for pt in path:
         diff = (np.array(pt)-ne.pos)/ne.k1
         ne.step(*diff)
-    history.starts.append(start)
-    history.paths.append(path)
-policy = train_policy(history)
-for i in range(300):
-    action = policy.predict(ne.pos)
-    ne.step(*action)
+    print("Goal distance", np.linalg.norm(ne.pos-goal))
+    print_path_stats(path)
+
+    
+    if np.linalg.norm(ne.pos-goal) < thresh:
+        print("Found one close enough to add")
+        history.starts.append(start)
+        history.paths.append(path)
+
+ne.pos = np.array(starts[i])
+ne.path_color = (190,0,0,1)
+imitation_path = imitate_history(history, ne.pos)
+print_path_stats(imitation_path)
+for pt in imitation_path:
+    diff = (np.array(pt)-ne.pos)/ne.k1
+    ne.step(*diff)
 import ipdb; ipdb.set_trace()
+
+#policy = train_policy(history)
+#for i in range(300):
+#    action = policy.predict(ne.pos)
+#    ne.step(*action)
 
 
