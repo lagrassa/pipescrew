ddad21d801ed6d874afd12c04a824be483bb3051
Alex LaGrassa <lagrassa@mit.edu>
2019-11-12 10:11:11 -0500

    last of kinematic 2D world

--- scripts/nav_env.py
+++ scripts/nav_env.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+#convert to Box2D
 
 """
 2D gridworld. obstacles are represented as Obstacle
@@ -17,30 +18,31 @@ class NavEnv():
         self.goal = np.array((200,300))
         self.start = np.array((0,5)) + 5*np.random.random((2,))
         self.pos = self.start[:]
-        self.k1 = 1.3#
+        self.vel = np.array([0,0])
         self.pos_history = []
         self.path_color = (1,0,0,1)
-        self.max_vel = 10
-        self._k2 = 0.4#0.5# another secret constant
         self.obstacles = [(Obstacle(np.array((20,20)),50))]
-    
+        self.m = 1
+        self.mu = 0.5
+        self.dt = 0.01
+        for obs in self.obstacles:
+            obs.render(self.screen)
+    '''
+    Rolls dynamical system 1 dt according to x'', y''
+    ''' 
     def step(self,x,y):
         old_pos = self.pos[:]
         move = np.array((x,y))
-        #move cannot exceeed max vel
-        if np.linalg.norm(move) > self.max_vel:
-            move = (move / np.linalg.norm(move))*self.max_vel
-        if self.slip:
-            vp = 0.1
-            if len(self.pos_history) == 0:
-                vel = 0
-            else:
-                vel = move + self.pos - self.pos_history[-1]
-            new_pos = self.pos + self.k1*(move)+np.multiply(vp*vel,self._k2*(move**2))
+        if np.linalg.norm(move) > self.mu*self.m:
+            net_force = np.array([0,0])
         else:
-            new_pos = move + self.pos
+            net_force = move - np.sign(move)*(self.mu*self.m)
+        new_pos = old_pos + self.vel*self.dt + 0.5*net_force*self.dt**2
+        self.vel = self.vel+ move*self.dt
         if not self.collision_fn(new_pos):
             self.pos = new_pos
+        else:
+            self.vel = 0
         self.pos_history.append(new_pos[:])
         pygame.draw.line(self.screen,self.path_color,old_pos.astype(np.int32),new_pos.astype(np.int32), 5)
         self.render()
@@ -62,8 +64,6 @@ class NavEnv():
         goal_rect = pygame.Rect(self.goal[0],self.goal[1], view_wid, view_wid)
         pygame.draw.rect(self.screen, (170,0,0,1),start_rect,0)
         pygame.draw.rect(self.screen, (0,170,0,1),goal_rect,0)
-        for obs in self.obstacles:
-            obs.render(self.screen)
         pygame.display.flip()
 
 class Obstacle():
@@ -72,9 +72,14 @@ class Obstacle():
         self.w = w
     def render(self,screen):
         py_rect = pygame.Rect(self.origin[0], self.origin[1],self.w,self.w)
-        pygame.draw.rect(screen, (0,1,0,1),py_rect,0)
+        pygame.draw.rect(screen, (50,0,0,1),py_rect,0)
     def in_collision(self,pt):
         if (pt > self.origin).all() and (pt < self.origin + self.w).all():
             return True
         return False
+
+if __name__ == "__main__":
+    ne = NavEnv() 
+    import ipdb; ipdb.set_trace()
+    ne.step(0,0)
         
