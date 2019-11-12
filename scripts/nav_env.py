3e5a9f74ad468e45346e523f484ae180d008dd0f
Alex LaGrassa <lagrassa@mit.edu>
2019-11-12 13:39:53 -0500

    two different types of slippery envs, plus the 2 door obstacle setup

--- scripts/nav_env.py
+++ scripts/nav_env.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+from Box2D import *
 #convert to Box2D
 
 """
@@ -11,51 +12,74 @@ class NavEnv():
     def __init__(self, slip=True ):
         self.slip=slip
         self.gridsize = [300,400]
+        self.m = 1
+        self.mu = -1
+        self.ppm = 10.
+        self.joint = None
+        self.ice_boundary_x = 150./self.ppm
         pygame.init()
         self.screen = pygame.display.set_mode(self.gridsize)
         self.screen.fill((255,255,255))
         pygame.display.flip()
         self.goal = np.array((200,300))
         self.start = np.array((0,5)) + 5*np.random.random((2,))
-        self.pos = self.start[:]
-        self.vel = np.array([0,0])
+        self.world = b2World(gravity=(0,0), doSleep=True)
+        self.agent = self.world.CreateDynamicBody(
+            position=self.start,
+            fixtures=b2FixtureDef(
+                shape=b2PolygonShape(box=(3, 3))
+            )
+        )
+        self.ground = self.world.CreateStaticBody(
+            shapes=b2PolygonShape(box=(self.gridsize[0]/self.ppm, self.gridsize[1]/self.ppm))
+        )
+        self.check_and_set_mu(dirty_bit = True)
         self.pos_history = []
         self.path_color = (1,0,0,1)
-        self.obstacles = [(Obstacle(np.array((20,20)),50))]
-        self.m = 1
-        self.mu = 0.5
-        self.dt = 0.01
+        gap = 25
+        length = 90
+        dist_down=150
+        self.obstacles = [Obstacle(np.array((1,dist_down)),length,10),
+                           Obstacle(np.array((length+gap,dist_down)),length,10),
+                           Obstacle(np.array((2*length+2*gap,dist_down)),length,10),
+        ]
+        self.dt = 0.1
         for obs in self.obstacles:
             obs.render(self.screen)
+
+    def check_and_set_mu(self,dirty_bit = False):
+        old_pos = np.array(self.agent.position.tuple)
+        no_ice_mu = 0.9
+        ice_mu = 0.05
+        if old_pos[0] < self.ice_boundary_x:
+            if self.mu != no_ice_mu:
+                self.mu = no_ice_mu
+                dirty_bit = True
+        else:
+            if self.mu != ice_mu:
+                self.mu = ice_mu
+                dirty_bit = True
+        if dirty_bit:
+            if self.joint is not None:
+                self.world.DestroyJoint(self.joint) 
+            self.joint = self.world.CreateFrictionJoint(bodyA = self.agent, bodyB = self.ground,maxForce = self.m*self.mu )
+
     '''
     Rolls dynamical system 1 dt according to x'', y''
     ''' 
     def step(self,x,y):
-        old_pos = self.pos[:]
+        old_pos = np.array(self.agent.position.tuple)
+        self.check_and_set_mu()
         move = np.array((x,y))
-        if np.linalg.norm(move) > self.mu*self.m:
-            net_force = np.array([0,0])
-        else:
-            net_force = move - np.sign(move)*(self.mu*self.m)
-        new_pos = old_pos + self.vel*self.dt + 0.5*net_force*self.dt**2
-        self.vel = self.vel+ move*self.dt
-        if not self.collision_fn(new_pos):
-            self.pos = new_pos
-        else:
-            self.vel = 0
-        self.pos_history.append(new_pos[:])
-        pygame.draw.line(self.screen,self.path_color,old_pos.astype(np.int32),new_pos.astype(np.int32), 5)
+        self.agent.ApplyForceToCenter(force=move,wake = True) 
+        self.world.Step(self.dt, 6, 2)
+        new_pos = np.array(self.agent.position.tuple)
+        print(new_pos)
+        self.world.ClearForces()
+        self.pos_history.append(new_pos)
+        import ipdb; ipdb.set_trace()
         self.render()
 
-    def go_to(self, x,y):
-        self.step(*np.array([x,y])-self.pos)
-
-    def collision_fn(self,pt):
-        if pt[0] < 0 or pt[1] < 0:
-            return True
-        if pt[1] > self.gridsize[1] or pt[0] > self.gridsize[0]:
-            return True
-        return np.array([obs.in_collision(pt) for obs in self.obstacles]).any()
 
 
     def render(self):
@@ -64,22 +88,25 @@ class NavEnv():
         goal_rect = pygame.Rect(self.goal[0],self.goal[1], view_wid, view_wid)
         pygame.draw.rect(self.screen, (170,0,0,1),start_rect,0)
         pygame.draw.rect(self.screen, (0,170,0,1),goal_rect,0)
+        for i in range(len(self.pos_history)-1):
+            pygame.draw.line(self.screen,self.path_color,(self.ppm*self.pos_history[i]).astype(np.int32),(self.ppm*self.pos_history[i+1]).astype(np.int32), 5)
         pygame.display.flip()
 
 class Obstacle():
-    def __init__(self, origin, w):
+    def __init__(self, origin, w,y):
         self.origin = origin
-        self.w = w
+        self.x = w
+        self.y = y
     def render(self,screen):
-        py_rect = pygame.Rect(self.origin[0], self.origin[1],self.w,self.w)
+        py_rect = pygame.Rect(self.origin[0], self.origin[1],self.x,self.y)
         pygame.draw.rect(screen, (50,0,0,1),py_rect,0)
     def in_collision(self,pt):
-        if (pt > self.origin).all() and (pt < self.origin + self.w).all():
+        if (pt > self.origin).all() and (pt < self.origin + np.array(self.x, self.y)).all():
             return True
         return False
 
 if __name__ == "__main__":
     ne = NavEnv() 
-    import ipdb; ipdb.set_trace()
-    ne.step(0,0)
+    for i in range(50):
+        ne.step(0,3)
         
