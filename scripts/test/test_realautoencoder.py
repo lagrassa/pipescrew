1829baddb52a3626e611ab974680edfbf824ef69
guy <stevenl3@andrew.cmu.edu>
2020-02-13 17:15:50 -0500

    pretty well working dae on that one image

--- scripts/test/test_realautoencoder.py
+++ scripts/test/test_realautoencoder.py
@@ -2,9 +2,11 @@ from modelfree import vae
 import numpy as np
 from skimage.color import rgb2hsv
 import imageio
+from PIL import Image
 image =imageio.imread("encoder_test_img_Color.png")
-image = rgb2hsv(image)
+image = Image.fromarray(image).resize((80,50))
 
+image = rgb2hsv(image)
 image = image[:,:,0]
 image = image.reshape(image.shape+(1,)) #compatible with 3 channels
 n_images = 8
@@ -14,6 +16,6 @@ for i in range(n_images):
 data = data.reshape((data.shape[0], data.shape[1]*data.shape[2] *data.shape[3]))
 #my_vae, encoder, decoder, inputs, outputs, output_tensors = vae.make_vae(original_dim=image.shape[0] * image.shape[1])
 my_vae, encoder, decoder, inputs, outputs, output_tensors = vae.make_dsae(image.shape[0], image.shape[1], n_channels = image.shape[-1])
-n_train = 10
+n_train = 20
 vae.train_vae(my_vae, data, n_train, inputs, outputs, output_tensors)
-encoded = encoder(data)
+my_vae.save_weights("test_weights.h5y")
