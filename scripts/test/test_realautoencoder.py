6365839fd5546438de49fe4dfa8005e0e5ce1fb1
Alex LaGrassa <lagrassa@mit.edu>
2020-02-13 14:32:46 -0500

    deep spatial autoencoder trains!!

--- scripts/test/test_realautoencoder.py
+++ scripts/test/test_realautoencoder.py
@@ -12,8 +12,8 @@ data = np.zeros((n_images,)+image.shape)
 for i in range(n_images):
     data[i,:,:,:] = image
 data = data.reshape((data.shape[0], data.shape[1]*data.shape[2] *data.shape[3]))
-my_vae, encoder, decoder, inputs, outputs, output_tensors = vae.make_vae(original_dim=image.shape[0] * image.shape[1])
-#my_vae, encoder, decoder, inputs, outputs, output_tensors = vae.make_dsae(image.shape[0], image.shape[1], n_channels = image.shape[-1])
+#my_vae, encoder, decoder, inputs, outputs, output_tensors = vae.make_vae(original_dim=image.shape[0] * image.shape[1])
+my_vae, encoder, decoder, inputs, outputs, output_tensors = vae.make_dsae(image.shape[0], image.shape[1], n_channels = image.shape[-1])
 n_train = 10
 vae.train_vae(my_vae, data, n_train, inputs, outputs, output_tensors)
 encoded = encoder(data)
