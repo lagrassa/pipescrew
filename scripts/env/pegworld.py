e1f513ea9529e9665056ce4f6de6443406ed5953
guy <stevenl3@andrew.cmu.edu>
2020-02-24 21:56:08 -0500

    made pegworld much closer to the real world

--- scripts/env/pegworld.py
+++ scripts/env/pegworld.py
@@ -30,8 +30,10 @@ class PegWorld():
         if not self.handonly:
             #self.robot = p.loadURDF(os.environ["HOME"]+"/ros/src/franka_ros/franka_description/robots/model.urdf") #fixme, point somewhere less fragile
             self.robot = p.loadURDF("../../models/robots/model.urdf")
-            set_point(self.robot, (-0.4,0,0.005))
+            set_point(self.robot, (0,0,0.005))
             start_joints = (0.09186411075857098, 0.02008522792588543, 0.03645461729775788, -1.9220854528910314, 0.213232566443952983, 1.647271913704007, 0.0, 0.0, 0.0)
+            start_joints = [ 2.78892560e-04, -7.85089406e-01,  4.81729135e-05, -2.35613802e+00,
+                            4.95981896e-04,  1.57082514e+00,  7.85833531e-01, 0, 0]
             self.grasp_joint = 9
             p.changeDynamics(self.robot, -1, mass=0)
             ut.set_joint_positions(self.robot, ut.get_movable_joints(self.robot), start_joints)
@@ -46,23 +48,31 @@ class PegWorld():
     """
     table with a hollow and solid cylinder on it
     """
-    def setup_workspace(self, rectangle_loc = ((0,-0.2,0), (1,0,0,0)),
-            circle_loc = ((0,0.1,0),(1,0,0,0)),
-            obstacle_loc = ((0,0.05,0), (1,0,0,0)), board_loc = ((0,0,0),(1,0,0,0))):
-        self.floor = p.loadURDF("../../models/short_floor.urdf")
+    def setup_workspace(self, rectangle_loc = ([0.562, 0.151, 0.016], [-0.029, -0.   ,  0.   ,  1.   ]),
+            circle_loc = ([0.425, 0.101, 0.01 ],[-0.03, -0.  ,  0.  ,  1.  ]),
+            obstacle_loc = ([ 0.53172045, -0.03062703,  0.07507126], [-0.028, -0.   ,  0.   ,  1.   ]), board_loc = ((0.479, 0.0453, -0.015),[0.707, 0.707, 0.   , 0.   ])):
+        import ipdb; ipdb.set_trace()
+        #RigidTransform(rotation=np.array([[-5.78152806e-02, -9.98327119e-01,  4.84639353e-07],
+        #       [-9.98327425e-01,  5.78157598e-02,  3.97392158e-08],
+        #              [ 4.07518811e-07, -6.59092487e-08, -9.99999635e-01]]), translation=np.array([ 0.53810962,  0.08998347, -0.00768057]), from_frame='peg_center', to_frame='world')
+        #hole_pose = (np.array([ 0.53810962,  0.08998347, -0.00768057]), np.array([-2.89198577e-02, -9.19833769e-08,  1.37694750e-07,  9.99581685e-01]))
+        #self.floor = p.loadURDF("../../models/short_floor.urdf")
         #make board
-        width = 0.3
-        length = 0.2
+        width = 0.4
+        length = 0.3
         height = 0.02
         block_height = 0.01
-        self.board = ut.create_box(width, length, height) 
+        self.board = ut.create_box(width, length, height, color = (1,0.7,0,1)) 
         ut.set_pose(self.board, board_loc)
         #make circle
-        radius = 0.02
-        self.circle = ut.create_cylinder(radius, block_height)
+        radius = 0.078/2
+        self.circle = ut.create_cylinder(radius, block_height, color = (1,1,0,1))
         #make rectangle
-        self.rectangle =  ut.create_box(0.04, 0.07, block_height, color=(0,0,1,1))
-        self.obstacle = ut.create_box(0.11, 0.11, 0.06, color = (0.5,0.5,0.5,1))
+        self.rectangle =  ut.create_box(0.092, 0.069, block_height, color=(0.5,0,0.1,1))
+        self.obstacle = ut.create_box(0.08, 0.04, 0.08, color = (0.5,0.5,0.5,1))
+        rectangle_loc[0][-1] -= 0.5*block_height
+        circle_loc[0][-1] -= 0.5*block_height
+        obstacle_loc[0][-1] -= 0.5*0.08
         ut.set_pose(self.rectangle, rectangle_loc)
         ut.set_pose(self.circle, circle_loc)
         ut.set_pose(self.obstacle, obstacle_loc)
@@ -119,7 +129,8 @@ if __name__ == "__main__":
     circle_loc = ((0.3,0.2,0),(1,0,0,0)) #acquire from darknet
     obstacle_loc = ((0.2,0.1,0),(1,0,0,0)) #TODO acquire from perception
     rectangle_loc = ((0.3,0.5,0),(1,0,0,0)) 
-    pw = PegWorld(rectangle_loc=rectangle_loc, circle_loc=circle_loc, board_loc=board_loc, obstacle_loc=obstacle_loc, visualize=False, handonly=False)
+    pw = PegWorld(rectangle_loc=rectangle_loc, circle_loc=circle_loc, board_loc=board_loc, obstacle_loc=obstacle_loc, visualize=True, handonly=False)
+    import ipdb; ipdb.set_trace()
     #pw = PegWorld(visualize=False, handonly = False)
     pw.attach_shape('rectangle', (np.array([0,0,0.05]), np.array([1,0,0,0])))
     traj = pw.make_traj(np.array([0,0.05,0.3]))
