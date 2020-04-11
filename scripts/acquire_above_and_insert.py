1dac37bf0109a2f2f09432d118a54d19e27711da
Alex LaGrassa <lagrassa@cmu.edu>
2020-04-11 15:16:37 -0400

    changed utility script

--- scripts/acquire_above_and_insert.py
+++ scripts/acquire_above_and_insert.py
@@ -1,13 +1,15 @@
 from frankapy import FrankaArm
+print("imported franka arm")
 import numpy as np
-
 fa = FrankaArm()
-fa.apply_effector_forces_torques(args.time, 0, 0, 0)
+print("made franka arm")
 input("Bring arm to where it should take the gripper:")
+fa.apply_effector_forces_torques(15, 0, 0, 0)
 rt = fa.get_pose()
-np.save("above_pose.npy", (rt.translation,rt.rotation))
-input("Bring arm to where insertion point:")
-rt = fa.get_pose()
-np.save("insert_pose.npy", (rt.translation, rt.rotation))
+print(rt)
+np.save("data/grasp_pipe_pose_trans.npy", rt.translation)
+#input("Bring arm to where insertion point:")
+#rt = fa.get_pose()
+#np.save("insert_pose_trans.npy", rt.translation)
 
 
