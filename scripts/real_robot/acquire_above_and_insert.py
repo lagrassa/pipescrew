bee287561696590705899b82963a5dcd5316e911
Alex LaGrassa <lagrassa@cmu.edu>
2020-04-11 15:17:22 -0400

    changed utility script

--- scripts/real_robot/acquire_above_and_insert.py
+++ scripts/real_robot/acquire_above_and_insert.py
@@ -2,7 +2,12 @@ from frankapy import FrankaArm
 import numpy as np
 
 fa = FrankaArm()
-fa.apply_effector_forces_torques(args.time, 0, 0, 0)
+import ipdb; ipdb.set_trace()
+for i in range(30):
+    fa.apply_effector_forces_torques(1, 0, 0, 0)
+    print(fa.get_pose().euler_angles)
+
+
 input("Bring arm to where it should take the gripper:")
 rt = fa.get_pose()
 np.save("above_pose.npy", (rt.translation,rt.rotation))
